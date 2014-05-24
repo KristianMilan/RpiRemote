d8aa62fe34bf70322032c0c66461bb406c25a393
pabloxxl <pabloxxl@fastmail.fm>
2014-05-24 19:57:12 +0200

    Created config file for Client side.

--- Client/main.py
+++ Client/main.py
@@ -2,6 +2,9 @@
 import kivy
 from kivy.app import App
 from kivy.uix.button import Button
+
+from util import Config
+from util import RpiConnect
 #IMPORTS
 
 kivy.require('1.0.7')
@@ -12,4 +15,7 @@ class Remote(App):
         return Button(text='Play')
 
 if __name__ == '__main__':
+    conf = Config("config")
+    conf.parseConfig()
+    conn = RpiConnect(conf.ip,conf.port)
     Remote().run()
--- Client/util.py
+++ Client/util.py
@@ -1,11 +1,44 @@
 #IMPORTS
 from socket import *
+from ConfigParser import ConfigParser
 #IMPORTS
 
+class Config(object):
+  def __init__(self,path):
+    self.parser = ConfigParser()
+    if self.parser.read(path):
+      self.parsed = True
+    else:
+      self.parsed = False
+      print "Could not read config file"
+  def parseConfig(self):
+    if self.parsed:
+      self.__parseIp()
+      self.__parsePort()
+    else:
+      self.ip = "127.0.0.1"
+      self.port = 5000
+
+  def __parseIp(self):
+    try:
+      self.ip = self.parser.get("config","ip")
+    except:
+      self.ip = "127.0.0.1" 
+      print "Could not parse ip adress"
+
+  def __parsePort(self):
+    try:
+      sPort = self.parser.get("config","port")
+      self.port = int(sPort)
+    except:
+      self.port = 5000
+      print "Could not parse port"
+
+		
 class RpiConnect(object):
-  IP = "192.168.1.124" #IP Adress of Rpi
-  PORT = 5003 #Port of Rpi
-  def __init__(self):
+  def __init__(self,ip,port):
+    self.ip = ip
+    self.port = port
     self.sock = socket(AF_INET, SOCK_STREAM)
 
   def connect(self):
